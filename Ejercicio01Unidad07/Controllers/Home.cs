7d13298105718fd899910d3b73c178e1356b09b4
hmartin <hmartin@107-17>
2024-10-17 14:39:56 +0200

    Agregar archivos de proyecto.

---
+++ Ejercicio01Unidad07/Controllers/Home.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ejercicio01Unidad07.Controllers
+{
+    public class Home : Controller
+    {
+       public ActionResult Index()
+        {
+            String texto;
+            if (DateTime.Now.Hour > 6 && DateTime.Now.Hour >= 12)
+            {
+                texto = "Buenos Dias";
+            }
+            else if (DateTime.Now.Minute > 12 && DateTime.Now.Hour >= 19)
+            {
+                texto = "Buenas Tardes";
+            }
+            else
+            {
+                texto = "Buneas Noches";
+            }
+
+
+
+            ViewData["Nombre"] = texto;
+            ViewBag.hora=DateTime.Now.Hour;
+            return View(); 
+        
+        }
+
+        
+    }
+}
---
+++ Ejercicio01Unidad07/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Ejercicio01Unidad07.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
